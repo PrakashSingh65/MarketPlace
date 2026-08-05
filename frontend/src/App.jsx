9aff68ca0ce97edf94fb02d2af3adf4e720280dc
PrakashSingh65 <pratapsing5656@gmail.com>
2026-08-05 15:36:10 +0530

    Fix import statements and enhance App component structure with CartProvider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,28 +10,33 @@ import ProtectedRoute from './components/ProtectedRoute';
 import Home from './pages/Home';
 import AIAssistant from './components/AIAssistant';
 import ProductDetail from './pages/ProductDetail';
-import profile from './pages/profile';
-import Onbording from './pages/Onboarding';
+import Profile from './pages/Profile';
+import Onboarding from './pages/Onboarding';
+import { CartProvider } from './context/cartContext';
+import Cart from './pages/cart';
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-slate-50 from-slate-50 to-slate-100 text-slate-800 flex flex-col font-sans">
-      <Navbar />
-      <main className="flex-1">
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/marketplace" element={<Marketplace />} />
-          <Route path="/supplier-dashboard" element={<ProtectedRoute allowedRole="SUPPLIER"><SupplierDashboard /></ProtectedRoute>} />
-          <Route path="/product/:id" element={<ProductDetail />} />
-          <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-          <Route path="/profile" element={<ProtectedRoute><profile /></ProtectedRoute>} />
-          <Route path="/onboarding" element={<Onbording />} />
-        </Routes>
-      </main>
-      <Footer />
-      <AIAssistant />
-    </div>
+    <CartProvider>
+      <div className="min-h-screen bg-slate-50 from-slate-50 to-slate-100 text-slate-800 flex flex-col">
+        <Navbar />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/marketplace" element={<Marketplace />} />
+            <Route path="/supplier-dashboard" element={<ProtectedRoute allowedRole="SUPPLIER"><SupplierDashboard /></ProtectedRoute>} />
+            <Route path="/product/:id" element={<ProductDetail />} />
+            <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+            <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+            <Route path="/onboarding" element={<Onboarding />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </main>
+        <Footer />
+        <AIAssistant />
+      </div>
+    </CartProvider>
   );
 }

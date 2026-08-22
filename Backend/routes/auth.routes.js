8dc1dcb19a10b08e27c732461bd7cf6d9046881d
PrakashSingh65 <pratapsing5656@gmail.com>
2026-08-22 11:43:27 +0530

    feat: remove unused authentication and product routes from the backend

--- Backend/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.post('/register', (req, res) => { /* Register controller logic */ });
-router.post('/login', (req, res) => { /* Login controller logic */ });
-router.get('/me', (req, res) => { /* Current user profile */ });
-
-module.exports = router;
--- Backend/routes/product.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => { /* Fetch all products */ });
-router.get('/:id', (req, res) => { /* Get product details */ });
-router.post('/', (req, res) => { /* Add new product */ });
-
-module.exports = router;
